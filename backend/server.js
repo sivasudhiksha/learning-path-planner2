18e189a666038fd921879aaaa0c1c33db738400d
sivasudhiksha <sivasudhikshaj@gmail.com>
2026-04-06 14:09:37 +0530

    fix: updated catch-all route for Express 5 compatibility

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("(.*)", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
   });
 } else {
